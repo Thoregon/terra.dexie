352c5ace26ba66ce90e9d33e9fac1f5b7e43ae14
Bernhard Lukassen <bl@bernhard-lukassen.com>
2019-12-29 12:59:33 +0100

    work on system structure, setup and boot

---
+++ index.mjs
@@ -0,0 +1,9 @@
+/**
+ * Adapters for object and event store with dexie (index DB)
+ *
+ * @author: Bernhard Lukassen
+ */
+
+export { default as DexieObjectStore }  from './lib/dexieobjectstore.mjs';
+export { default as DexieEventStore }   from './lib/dexieeventstore.mjs';
+
